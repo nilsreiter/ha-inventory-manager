c7893388892bfed8e7c5a45da563ed96a87a89c1
Nils Reiter <nils.reiter@uni-koeln.de>
2026-01-13 06:13:54 +0000

    setting attributes directly

--- custom_components/inventory_manager/number.py
+++ custom_components/inventory_manager/number.py
@@ -110,9 +110,11 @@ class InventoryNumber(InventoryManagerEntity, RestoreNumber, metaclass=ABCMeta):
         self.unique_id: str = entity_config[UNIQUE_ID]
 
         self._available: bool = True
-        self.native_unit_of_measurement = item.data.get(CONF_ITEM_UNIT, UNIT_PCS)
-        self.native_step = 0.25
-        self.native_min_value = 0
+        self._attr_native_unit_of_measurement = item.config_entry.data.get(
+            CONF_ITEM_UNIT, UNIT_PCS
+        )
+        self._attr_native_step = 0.25
+        self._attr_native_min_value = 0
 
     @property
     def native_value(self) -> float:
